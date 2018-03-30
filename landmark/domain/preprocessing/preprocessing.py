b2c4f2057b79f50f4e31ec42d161da134b745e37
ArthurOuaknine <arthur.ouaknine@gmail.com>
2018-03-30 16:14:27 +0200

    fix: resize function using opencv and drop unfound images
    TODO: record droped images
    
    Issue #2

--- landmark/domain/preprocessing/preprocessing.py
+++ landmark/domain/preprocessing/preprocessing.py
@@ -2,14 +2,15 @@
 
 import dask
 import numpy as np
-from PIL import Image
-from PIL import ImageFile
+import cv2
+# from PIL import Image
+# from PIL import ImageFile
 
-ImageFile.LOAD_TRUNCATED_IMAGES = True # helps for trancated images
+# ImageFile.LOAD_TRUNCATED_IMAGES = True # helps for trancated images
 
 class LandmarkImage(object):
     """Class to load and preprocess an image"""
-    SIZE = (200, 200, 3)
+    SIZE = (500, 500)
 
     def __init__(self, path):
         self.path = path
@@ -21,11 +22,11 @@ class LandmarkImage(object):
 
     @property
     def _load(self):
-        img = Image.open(self.path)
+        # img = Image.open(self.path)
         # img.thumbnail(self.SIZE, Image.ANTIALIAS)
-        img = np.asarray(img)
-        import ipdb; ipdb.set_trace()
-        img = np.reshape(img, self.SIZE)
+        # img = np.asarray(img)
+        img = cv2.imread(self.path)
+        img = cv2.resize(img, dsize=self.SIZE, interpolation=cv2.INTER_CUBIC)
         return img
 
 
@@ -50,11 +51,12 @@ def _preprocess(path):
     # return LandmarkImage(path).preprocess
     try:
         return LandmarkImage(path).img
-    except FileNotFoundError:
-        print("%s doesn't exists and cannot be loaded. Returning None." % path)
+    except (FileNotFoundError, cv2.error) as e:
+        print("File not found: %s" % path)
 
 def _parallel_load(paths):
-    # all_paths = [_get_path(path) for path in paths]
     album = [_preprocess(path) for path in paths["path"]]
     album = dask.compute(album)[0]
-    return np.array(album)
+    cleaned_album = [img for img in album if img is not None]
+    cleaned_album = np.expand_dims(cleaned_album, axis=0)[0]
+    return cleaned_album
