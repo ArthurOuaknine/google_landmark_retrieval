fb447e8fa11d802a4ac9cc1cbbb829191d62cd74
ArthurOuaknine <arthur.ouaknine@gmail.com>
2018-04-20 18:21:01 +0200

    feat: start creating similarities
    Issue #4

---
+++ landmark/domain/sift/similarities.py
@@ -0,0 +1,23 @@
+"""Class to compute similarities using sift features"""
+
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+from landmark.infrastructure.sift.sift_mean_dataset import SiftMean
+from landmark.infrastructure.submission_format import Submission
+
+class Similarities(object):
+
+    def __init__(self, path_to_config):
+        self.path_to_config = path_to_config
+        self.data = SiftMean(self.path_to_config).test
+        self.indexes = np.array(self.data.index)
+
+    def neighborhood(self, n_neighbors):
+        nbrs = NearestNeighbors(n_neighbors=n_neighbors, algorithm='ball_tree').fit(self.data)
+        _, indices = nbrs.kneighbors(self.data)
+        neighbors = self.indexes[indices]
+        neighbors = [[list(neighbor)] for neighbor in neighbors]
+        results = pd.DataFrame(neighbors, index=self.indexes)
+        submission = Submission(results)
+        submission.export(self.path_to_config, file_name="unkwnown.csv")
