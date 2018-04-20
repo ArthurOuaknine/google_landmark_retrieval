9d120707ede28f2ab4defb7a084ceaf6aff68cab
ArthurOuaknine <arthur.ouaknine@gmail.com>
2018-04-20 12:49:02 +0200

    feat: add Batch class to modify the sift parallel
    Issue #4

---
+++ landmark/utils/batch.py
@@ -0,0 +1,49 @@
+"""Class to create batch of data"""
+import pandas as pd
+
+class Batch(object):
+
+    def __init__(self, batch_size, data, label=None, nb_epoch=1):
+        self.data = data
+        self.label = label
+        self.nb_data = data.shape[0]
+        self.batch_size = batch_size
+        self.nb_epoch = 1
+        self.current_min = 0
+        self.current_max = self.batch_size
+        self.batch_data = self.data[:batch_size]
+        if self.label is not None:
+            self.batch_label = self.label[:batch_size]
+
+    @property
+    def next(self):
+        if self.current_max > self.nb_data:
+            if self.label is None:
+                self.end(has_label=False)
+            else:
+                self.end(has_label=True)
+        else:
+            self.batch_data = self.data[self.current_min:self.current_max]
+            self.current_min += self.batch_size
+            self.current_max += self.batch_size
+
+    def end(self, has_label):
+        if self.nb_epoch != 1:
+            self.batch_data = self.data[self.current_min:]
+            if has_label:
+                self.batch_label = self.label[self.current_min:]
+            self.current_max = self.current_max - self.nb_data
+            self.batch_data = pd.concat([self.batch_data, self.data[:self.current_max]],
+                                        axis=1)
+            if has_label:
+                self.batch_label = pd.concat([self.batch_label, self.label[:self.current_max]],
+                                             axis=1)
+            self.current_min = self.current_max
+            self.current_max += self.batch_size
+
+        else:
+            self.batch_data = self.data[self.current_min:]
+            if has_label:
+                self.batch_label = self.label[self.current_min:]
+            self.current_min = 0
+            self.current_max = self.batch_size
