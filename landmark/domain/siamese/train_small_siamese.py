8a77d7c32ea126eaa1b3523de7cbaf7e2219bca0
ArthurOuaknine <arthur.ouaknine@gmail.com>
2018-05-10 10:35:49 +0200

    feat: first training (need more work)
    Issue #5

--- landmark/domain/siamese/train_small_siamese.py
+++ landmark/domain/siamese/train_small_siamese.py
@@ -1,10 +1,23 @@
 import os
 import pandas as pd
 import numpy as np
+import time
 from landmark.infrastructure.similarity_dataset import SimilarityDataset
 from landmark.domain.preprocessing.preprocessing_landmark_recognition import LandmarkRecognitionAlbum
 from landmark.domain.siamese.small_siamese import SmallSiamese
 from landmark.utils.batch import Batch
+
+def structure_data(batch_imgs, index_batch):
+    batch = [batch_im[index_batch] for batch_im in batch_imgs]
+    batch = np.expand_dims(batch, axis=0)[0]
+    return batch
+
+def structure_labels(data):
+    labels = pd.DataFrame(data["similarity"])
+    labels.columns = ["similarity"]
+    labels["similarity_env"] = 1 - labels["similarity"]
+    labels = labels.as_matrix()
+    return labels
 
 if __name__=="__main__":
     directory = os.environ["LANDMARK_HOME"]
@@ -21,6 +34,15 @@ if __name__=="__main__":
     batcher = Batch(batch_size, data, nb_epoch=nb_epoch)
 
     for i in range(nb_iter):
+        time1 = time.time()
         batch_data = batcher.batch_data
         batch_imgs, exceptions = LandmarkRecognitionAlbum(batch_data).load
-        import ipdb; ipdb.set_trace()
+        batch_x1 = structure_data(batch_imgs, 0)
+        batch_x2 = structure_data(batch_imgs, 1)
+        labels = structure_labels(batch_data)
+        loss = siamese.train(batch_x1, batch_x2, labels, i)
+        time2 = time.time()
+        print("***** Loss at step %s: %s *****" %(i, str(loss)))
+        print("===> Running time: %s" % (str(time2-time1)))
+        print("**********")
+        # TODO: split train/validation, create method for test and add accuracy to tensorboard
